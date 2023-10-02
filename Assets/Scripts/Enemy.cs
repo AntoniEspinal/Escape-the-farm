079c257bd59e360208bdf611e0bfc6b6ebbd380a
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2023-10-02 13:49:00 -0400

    ok

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private float speed = 3.0f;
+    public Rigidbody enemyRb;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
