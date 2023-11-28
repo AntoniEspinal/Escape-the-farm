23f757a2bb1f13904ffbcf8c191d5c13f1374fcd
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2023-11-28 13:46:37 -0500

    Antoni Ca

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -23,6 +23,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void OptionMenu()
+    {
+        SceneManager.LoadScene("OptionMenu");
+    }
     public void Quit()
     {
         Application.Quit();
