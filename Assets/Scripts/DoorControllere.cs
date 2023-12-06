edfd80da95b85514a24b1d20ed18dda822bf7b57
edwardrivera11 <98423321+edwardrivera11@users.noreply.github.com>
2023-12-06 14:14:07 -0500

    Thugging it out pt 2

---
+++ Assets/Scripts/DoorControllere.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControllere : MonoBehaviour
+{
+
+    Quaternion startAngle = Quaternion.Euler(0,0,0);
+    Quaternion endAngle = Quaternion.Euler(0,-90,0);
+    Quaternion currentAngle;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAngle = startAngle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDoor ()
+    {
+            ChangeCurrentAngle();
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentAngle, 0.5f);
+    }
+
+    void ChangeCurrentAngle()
+    {
+        if(currentAngle.eulerAngles.y == startAngle.eulerAngles.y) 
+        {
+           currentAngle = endAngle;
+        }else 
+        {   
+            currentAngle = startAngle;
+        }
+    }
+}
