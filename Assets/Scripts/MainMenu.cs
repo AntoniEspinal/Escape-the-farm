e1e29b3293b64b12fc2f435529ca9ecb564b82c8
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2024-05-14 14:04:05 -0400

    Character scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -28,6 +28,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("OptionMenu");
     }
+    public void Character()
+    {
+        SceneManager.LoadScene("Character");
+    }
     public void Quit()
     {
         Application.Quit();
